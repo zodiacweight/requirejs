1f69f79db83957164d2ef5213c3fdaaacf0564e7
zodiacbalance <zodiacweight@gmail.com>
2017-05-25 15:44:23 +0300

    Works without superfluous files

--- scripts/common.js
+++
@@ -1,9 +0,0 @@
-/*define(['scripts/subdued'],function (subdued) {
-    console.log('subdued: %c'+subdued, 'color:orange');
-    return {
-        getHello: function () {
-            return 'Hello World, you are a ';
-        },
-        subdued:subdued
-    };
-}); */
